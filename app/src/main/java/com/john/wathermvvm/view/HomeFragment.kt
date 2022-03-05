d6027bd630d18aab480deb35de8ad3440d3a7154
John De la cruz <johndelacruzm@gmail.com>
2022-03-05 14:07:41 -0500

    Update Overlay Map implementation

--- app/src/main/java/com/john/wathermvvm/view/HomeFragment.kt
+++ app/src/main/java/com/john/wathermvvm/view/HomeFragment.kt
@@ -1,44 +1,47 @@
 package com.john.wathermvvm.view
 
-import android.app.Activity
 import android.app.AlertDialog
-import android.content.Intent
 import android.location.Geocoder
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.launch
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.widget.Autocomplete
-import com.google.android.libraries.places.widget.AutocompleteActivity
-import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.john.wathermvvm.R
 import com.john.wathermvvm.repository.network.NetworkDataState
 import com.john.wathermvvm.databinding.HomeFragmentBinding
 import com.john.wathermvvm.model.City
 import com.john.wathermvvm.view.adapters.CitiesAdapter
 import com.john.wathermvvm.viewmodel.HomeViewModel
+import com.john.wathermvvm.xutil.AddressAutoComplete
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private val viewModel: HomeViewModel by viewModels()
-    private lateinit var binding : HomeFragmentBinding
+    private lateinit var binding: HomeFragmentBinding
     private lateinit var adapter: CitiesAdapter
+    private var launcher = registerForActivityResult(AddressAutoComplete()) { place ->
+
+        if (place != null) {
+            val geocoder = Geocoder(activity)
+            viewModel.setCity(place.address, geocoder)
+        }
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = HomeFragmentBinding.inflate(inflater,container,false)
+        binding = HomeFragmentBinding.inflate(inflater, container, false)
 
         return binding.root
     }
@@ -48,14 +51,14 @@ class HomeFragment : Fragment() {
 
         viewModel.getCurrentList()
         setObservers()
-        binding.searchView.setOnClickListener {openOverlayMap()}
+        binding.searchView.setOnClickListener { launcher.launch() }
 
 
     }
 
     private fun setObservers() {
-        viewModel.networkState.observe(viewLifecycleOwner,{
-            when(it){
+        viewModel.networkState.observe(viewLifecycleOwner) {
+            when (it) {
                 is NetworkDataState.Success<List<City>> -> {
                     isProgressBarVisible(false)
                     setContent(it.data)
@@ -71,14 +74,14 @@ class HomeFragment : Fragment() {
             }
 
 
+        }
 
-        })
-
-        viewModel.deleteState.observe(viewLifecycleOwner,{
-            when(it){
+        viewModel.deleteState.observe(viewLifecycleOwner) {
+            when (it) {
                 is NetworkDataState.Success<String> -> {
                     isProgressBarVisible(false)
-                    Toast.makeText(context,"The city was deleted successfully",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, "The city was deleted successfully", Toast.LENGTH_SHORT)
+                        .show()
                     viewModel.getCurrentList()
 
                 }
@@ -93,12 +96,11 @@ class HomeFragment : Fragment() {
             }
 
 
-
-        })
+        }
     }
 
     private fun isProgressBarVisible(b: Boolean) {
-        binding.progressBar.visibility = if(b) View.VISIBLE else View.GONE
+        binding.progressBar.visibility = if (b) View.VISIBLE else View.GONE
 
     }
 
@@ -113,9 +115,9 @@ class HomeFragment : Fragment() {
 
         adapter = CitiesAdapter(CitiesAdapter.WeatherListener({
             val bundleShop = Bundle()
-            bundleShop.putLong("cityId",it)
+            bundleShop.putLong("cityId", it)
             Navigation.findNavController(binding.root).navigate(R.id.toDetailFragment, bundleShop)
-        },{
+        }, {
             showDialog(it)
 
         }))
@@ -134,7 +136,7 @@ class HomeFragment : Fragment() {
                     viewModel.deleteUser(it)
                 }
             }
-            setNegativeButton("No"){_, _ ->
+            setNegativeButton("No") { _, _ ->
             }
 
             setCancelable(false)
@@ -143,34 +145,4 @@ class HomeFragment : Fragment() {
 
     }
 
-    private fun openOverlayMap() {
-        // Set the fields to specify which types of place data to return.
-        val fields = listOf(Place.Field.ADDRESS, Place.Field.NAME)
-        // Start the autocomplete intent.
-        val intent = Autocomplete.IntentBuilder(
-            AutocompleteActivityMode.OVERLAY, fields)
-            .build(requireContext())
-        startActivityForResult(intent, REQUEST_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                val place = Autocomplete.getPlaceFromIntent(data!!)
-                val geocoder = Geocoder(activity)
-                viewModel.setCity(place.address,geocoder)
-            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
-                val status = Autocomplete.getStatusFromIntent(data!!)
-                Log.i("HomeFragment",status.statusMessage.toString())
-                Toast.makeText(requireContext(), status.statusMessage, Toast.LENGTH_LONG).show()
-            }
-        }
-
-    }
-    companion object{
-        const val REQUEST_CODE = 1
-
-    }
 }
