d64d39c4197d0405a4de39a4e7e50f5f87b4cc66
John De la cruz <johndelacruzm@gmail.com>
2022-03-12 18:33:44 -0500

    Update Adapters

---
+++ app/src/main/java/com/john/wathermvvm/view/adapters/forecast/ForecastAdapter.kt
@@ -0,0 +1,44 @@
+package com.john.wathermvvm.view.adapters.forecast
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.john.wathermvvm.model.Forecast
+import com.john.wathermvvm.util.margins
+
+class ForecastAdapter : ListAdapter<Forecast, RecyclerView.ViewHolder>(MODEL_COMPARATOR)  {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ForecastViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val forecast = getItem(position)
+
+        (holder as ForecastViewHolder).bind(forecast)
+
+        if (position==0){
+            holder.itemView.margins(16,0,0,0)
+        }
+        if (position==currentList.size-1){
+            holder.itemView.margins(0,16,0,0)
+        }
+
+    }
+
+
+    companion object {
+        private val MODEL_COMPARATOR = object : DiffUtil.ItemCallback<Forecast>() {
+            override fun areItemsTheSame(oldItem: Forecast, newItem: Forecast): Boolean {
+                return (oldItem == newItem)
+            }
+
+            override fun areContentsTheSame(oldItem: Forecast, newItem: Forecast): Boolean =
+                oldItem == newItem
+        }
+    }
+
+
+
+}
